29552c740ed5c3fe92b133c8d0cac7b3dee84683
atruman843 <andy.truman843@gmail.com>
2018-02-05 21:32:15 -0500

    Update Hand.java

--- Poker/Hand.java
+++ Poker/Hand.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package poker;
 
-/**
- *
- * @author Andy
- */
 public class Hand {
     Card[] hand = new Card[5];
     private int index = 0;
