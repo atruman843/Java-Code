2fb306edf9cd168e57ad0a70ef021734468ba515
atruman843 <andy.truman843@gmail.com>
2018-02-05 21:32:37 -0500

    Update Deck.java

--- Poker/Deck.java
+++ Poker/Deck.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package poker;
 
-/**
- *
- * @author Andy
- */
+
 import java.util.Random;
 public class Deck {
     private Card[] deck = new Card[52];
