0cd28fc5f978f604386f9a8ecce4a63fb936e7b1
atruman843 <andy.truman843@gmail.com>
2018-02-05 21:33:22 -0500

    Update Card.java

--- Poker/Card.java
+++ Poker/Card.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package poker;
 
-/**
- *
- * @author Andy
- */
 public class Card {
     private int suit;
     private int rank;
